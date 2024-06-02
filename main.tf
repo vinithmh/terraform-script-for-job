e307f91175f1d4c392819d9ddad7f41989929ba6
vinithmh <vinithmh2000@gmail.com>
2024-06-03 00:37:19 +0530

    sdahgd

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_codecommit_repository" "revhire-job-repository" {
-  repository_name = "revhire-job-repository"
+  repository_name = "revhire-job-repository-auto"
   description     = "A revhire job-repository on AWS CodeCommit"
 }
 
 resource "aws_iam_role" "codebuild_role" {
-  name = "codebuild-service-role-for-job"
+  name = "codebuild-service-role-for-job-auto"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -27,7 +27,7 @@ data "aws_codecommit_repository" "revhire-job-repository" {
 }
 
 resource "aws_iam_role_policy" "codebuild_policy" {
-  name = "codebuild-policy"
+  name = "codebuild-policy-auto"
   role = aws_iam_role.codebuild_role.id
 
   policy = jsonencode({
@@ -131,7 +131,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_ssm_full_access" {
 }
 
 resource "aws_codebuild_project" "revhire-job-build" {
-  name          = "revhire-job-build"
+  name          = "revhire-job-build-auto"
   description   = "Build project for revhire-job application"
   service_role  = aws_iam_role.codebuild_role.arn
 
@@ -179,7 +179,7 @@ resource "aws_codebuild_project" "revhire-job-build" {
 }
 
 resource "aws_iam_role" "codepipeline_role" {
-  name = "codepipeline-service-role"
+  name = "codepipeline-service-role-auto"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -194,7 +194,7 @@ resource "aws_iam_role" "codepipeline_role" {
 }
 
 resource "aws_iam_role_policy" "codepipeline_policy" {
-  name = "codepipeline-policy"
+  name = "codepipeline-policy-auto"
   role = aws_iam_role.codepipeline_role.id
 
   policy = jsonencode({
@@ -236,7 +236,7 @@ resource "aws_iam_role_policy_attachment" "codepipeline_codebuild_developeracces
 }
 
 resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "revhire-job-codepipeline-artifacts"
+  bucket = "revhire-job-codepipeline-artifacts-auto"
   force_destroy = true
 }
 
@@ -262,7 +262,7 @@ resource "aws_s3_bucket_policy" "codepipeline_bucket_policy" {
 }
 
 resource "aws_codepipeline" "revhire_job_pipeline" {
-  name     = "revhire-job-pipeline"
+  name     = "revhire-job-pipeline-auto"
   role_arn = aws_iam_role.codepipeline_role.arn
 
   artifact_store {
